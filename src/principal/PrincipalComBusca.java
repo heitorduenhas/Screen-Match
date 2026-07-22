ae788ad128a76522a81024062e92bc3543cd1a8f
heitorduenhas <hassisduenhas@gmail.com>
2026-07-22 17:31:44 -0300

    Primeiro commit

---
+++ src/principal/PrincipalComBusca.java
@@ -0,0 +1,29 @@
+package principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o nome do filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String chave = "b6eb8eff";
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=" +chave;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+
+    }
+}
